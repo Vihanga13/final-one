48432477fedab8634bb95d05b4e28c3c59859697
Senapathi Vihanga Nilusha <vihaax23@gmail.com>
2025-05-13 21:56:30 +0530

    fix: Update minimum SDK version to 34 and change server IP in scan meal page

--- models-new/scanmeal_page.dart
+++ models-new/scanmeal_page.dart
@@ -81,7 +81,7 @@ class _ModernScanMealPageState extends State<ScanMealPage> with TickerProviderSt
       // Send image to your Flask API
       var request = http.MultipartRequest(
         'POST',
-        Uri.parse('http://192.168.1.209:5000/predict'), // Use your server IP
+        Uri.parse('http://192.168.8.107:5000/predict'), // Use your server IP
       );
       request.files.add(await http.MultipartFile.fromPath('file', image.path));
       var response = await request.send();
